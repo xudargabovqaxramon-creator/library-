5250b6aa060ac79b3aee02f981561e8a14c70ac3
xudargabovqaxramon-creator <xudargabovqaxramon@gmail.com>
2025-12-25 22:23:19 +0500

    init

--- router/upload.routes.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require("express");
-const uploadRouter = Router();
-const { uploadAuthorImage, uploadBookImage } = require("../controller/upload.controller");
-
-uploadRouter.post("/img_author", uploadAuthorImage);
-uploadRouter.post("/img_book", uploadBookImage);
-
-module.exports = uploadRouter;
